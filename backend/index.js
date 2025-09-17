04ceef332f7b710922376894f8ee8db26d1ae304
shkelzenkonxheli <xeni_kongjeli@hotmail.com>
2025-09-17 21:12:01 +0200

    Use Database_Url from env

--- backend/index.js
+++ backend/index.js
@@ -10,11 +10,8 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
 app.use(cors());
